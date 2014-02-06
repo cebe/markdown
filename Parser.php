0ec8506f9dfd8f6ecf05cdd44234323fd0e085ae
Carsten Brandt <mail@cebe.cc>
2014-02-06 15:53:30 +0100

    initial parser and readme

---
+++ Parser.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace cebe\markdown;
+
+/**
+ * A generic parser for markdown-like languages
+ *
+ * @author Carsten Brandt <mail@cebe.cc>
+ */
+class Parser
+{
+	/**
+	 * @var int the maximum nesting level for language elements
+	 */
+	public $maximumNestingLevel = 64;
+
+	/**
+	 * @var array a map of markers to parser methods
+	 */
+	protected $inlineMarkers = [];
+
+	/**
+	 * Parses the given text considering the full language.
+	 *
+	 * @param string $text the text to parse
+	 * @return string parsed markup
+	 */
+	public function parse($text)
+	{
+		$text = str_replace("\r\n", "\n", $text);// TODO speed benchmark preg_replace and str_replace
+		$text = str_replace("\r", "\n", $text);
+		$text = str_replace("\t", '    ', $text); // TODO preserve tabs in code blocks!
+
+		$lines = explode("\n", $text);
+
+		return $this->parseBlocks($lines);
+	}
+
+	/**
+	 * Parses a paragraph without block elements (block elements are ignored.
+	 *
+	 * @param string $text the text to parse
+	 * @return string parsed markup
+	 */
+	public function parseParagraph($text)
+	{
+		return $this->parseInline($text);
+	}
+
+	private $depth = 0;
+
+	/**
+	 *
+	 */
+	protected function parseBlocks($lines)
+	{
+		if ($this->depth++ > $this->maximumNestingLevel) {
+			// TODO just do not parse
+			throw new \Exception('Max nesting depth reached.');
+		}
+
+		$blocks = [];
+
+		// convert lines to blocks
+
+		for($i = 0; isset($lines[$i]); $i++) { // TODO benchmark isset() vs. $i < count($lines)
+			if (!empty($lines[$i])) { // skip empty lines
+				// identify a blocks beginning
+				$blockType = $this->identifyLine($lines, $i);
+
+				// call consume method for the detected block type to consume further lines
+				list($blocks[], $i) = $this->{'consume' . $blockType}($lines, $i);
+			}
+		}
+
+		// convert blocks to markup
+
+		$output = '';
+		foreach($blocks as $block) {
+			$output .= $this->{'render' . $block['type']}($block) . "\n";
+		}
+
+		$this->depth--;
+
+		return $output;
+	}
+
+	/**
+	 * Identifies a line as a block type
+	 *
+	 * @param $lines
+	 * @param $current
+	 * @return string the detected block type
+	 */
+	protected function identifyLine($lines, $current)
+	{
+		return 'paragraph';
+	}
+
+	/**
+	 * Consume lines for a paragraph
+	 *
+	 * @param $lines
+	 * @param $current
+	 * @return array
+	 */
+	public function consumeParagraph($lines, $current)
+	{
+		// consume until newline
+
+		$block = [
+			'type' => 'paragraph',
+			'content' => [],
+		];
+		for($i = $current; isset($lines[$i]); $i++) {
+			if (ltrim($lines[$i], ' ') !== '' && ($i === $current || $this->identifyLine($lines, $i) === 'paragraph')) {
+				$block['content'][] = $lines[$i];
+			} else {
+				break;
+			}
+		}
+
+		return [$block, $i];
+	}
+
+	/**
+	 * Render a paragraph block
+	 *
+	 * @param $block
+	 * @return string
+	 */
+	protected function renderParagraph($block)
+	{
+		return '<p>' . $this->parseInline(implode("\n", $block['content'])) . '</p>';
+	}
+
+	/**
+	 * Parses inline elements of the language.
+	 *
+	 * @param $text
+	 * @return string
+	 */
+	protected function parseInline($text)
+	{
+		$markers = $this->inlineMarkers;
+
+		$paragraph = '';
+
+		while(!empty($markers)) { // TODO benchmark empty vs. while($markers)
+			$closest = null;
+			$cpos = 0;
+			foreach($markers as $marker => $method) {
+				if (($pos = strpos($text, $marker)) === false) {
+					unset($markers[$marker]);
+					continue;
+				}
+
+				if ($closest === null || $pos < $cpos || ($pos === $cpos && strlen($marker) < strlen($closest))) {
+					$closest = $marker;
+					$cpos = $pos;
+				}
+			}
+
+			// add the text up to next marker to the paragraph
+			if ($cpos !== 0) {
+				$paragraph .= substr($text, 0, $cpos);
+				$text = substr($text, $cpos);
+			}
+
+			// parse the marker
+			if ($closest !== null) {
+				$method = $markers[$closest];
+				list($output, $offset) = $this->$method($text);
+				$paragraph .= $output;
+				$text = substr($text, $offset);
+			}
+		}
+
+		$paragraph .= $text;
+
+		return $paragraph;
+	}
+}
