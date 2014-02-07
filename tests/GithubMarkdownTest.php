c5bf5b7d7e6620e4f567a6b8d765958c02cbd78d
Carsten Brandt <mail@cebe.cc>
2014-02-07 12:53:10 +0100

    make github test work

--- tests/GithubMarkdownTest.php
+++ tests/GithubMarkdownTest.php
@@ -2,6 +2,10 @@
 
 namespace cebe\markdown\tests;
 
+if (!class_exists('cebe\markdown\tests\MarkdownTest')) {
+	require(__DIR__ . '/MarkdownTest.php');
+}
+
 /**
  * @author Carsten Brandt <mail@cebe.cc>
  */
